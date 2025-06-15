82fd106bcd58a6661fed72beb9ec196cd6386a5b
Triple3T <triple@triple-lab.com>
2025-06-15 09:28:26 +0900

    Update disallow-ssh-root.sh

--- disallow-ssh-root.sh
+++ disallow-ssh-root.sh
@@ -1,3 +1,5 @@
 # run as root!
-sed -e 's/^#\?PermitRootLogin yes.*$/PermitRootLogin no/' -i /etc/ssh/sshd_config
+grep -q '^[ \t]*#\?[ \t]*PermitRootLogin' /etc/ssh/sshd_config \
+  && sed -i -e 's/^[ \t]*#\?[ \t]*PermitRootLogin[ \t]*.*$/PermitRootLogin no/' /etc/ssh/sshd_config \
+  || echo 'PermitRootLogin no' >> /etc/ssh/sshd_config
 systemctl restart sshd
